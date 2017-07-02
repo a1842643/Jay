ba998db425983814844721b2c1b4752e0db65502
a1842643 <a1842643@gmail.com.tw>
2017-07-02 18:17:19 +0800

    建立ProductController並建立維護畫面Index

---
+++ ShoppingGo/ShoppingGo/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingGo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            //宣告回傳商品列表result
+            List<Models.Product> result = new List<Models.Product>();
+
+            //使用Entities
+            using (Models.ShoppingGoDataEntities db = new Models.ShoppingGoDataEntities())
+            {
+                //使用LinQ抓取目前Products資料庫的所有資料
+                result = (from s in db.Product select s).ToList();
+
+                //將result傳送給檢視
+                return View(result);
+
+            }
+        }
+    }
+}
