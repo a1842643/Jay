2fb59cdbf61cbb1bfc783750e80275e214f70e0a
a1842643 <a1842643@gmail.com.tw>
2017-07-03 14:51:45 +0800

    頁面資訊微調

---
+++ ShoppingGo/ShoppingGo.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingGo.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void Creat()
+        {
+
+        }
+    }
+}
